94b985e6e0572a13c072e8714eafc5ddb99a5ad0
DimaDostoevskiy <dostoevskiy@list.ru>
2022-11-04 17:34:34 +0300

    second prod commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5555;
+const cors = require('cors');
+
+//Константы
+const {CONNECT, DISCONNECT, SOCKET_USERS_CHANGES, INVITE_USER} = require("./src/constants/socketEvents");
+
+//socket.io инициация
+const { createServer } = require("http");
+const { Server } = require('socket.io');
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3001",
+    }
+});
+
+//Хранилище данных о состоянии пользователей
+const socketUsers = {};
+
+//Соединение пользователей онлайн и обработчики событий
+io.on(CONNECT, (socket) => {
+    socketUsers[socket.id] = jwtUser;
+    io.emit(SOCKET_USERS_CHANGES, socketUsers);
+
+    //Разрыв соединения сокета и удаления пользователя из списка онлайн
+    socket.on(DISCONNECT, () => {
+        delete socketUsers[socket.id];
+        io.emit(SOCKET_USERS_CHANGES, socketUsers);
+    });
+});
+
+app.use(cors());
+
+httpServer.listen(PORT,  () => {
+    console.log(`Blast-off on http://localhost:${PORT} pid:${process.pid}`);
+});
---
+++ src/constants/socketEvents.js
@@ -0,0 +1,6 @@
+module.exports = {
+    CONNECT: 'connection',
+    DISCONNECT: 'disconnect',
+    SOCKET_USERS_CHANGES: 'socket_users_changes',
+    INVITE_USER: 'invite_user'
+}
